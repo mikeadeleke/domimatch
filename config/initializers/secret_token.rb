e6cf49a6b0a2398bb46e6dce0e35d8da8eca7214
Mike Adeleke <madeleke13@gmail.com>
2013-11-18 16:38:50 -0600

    New application and added devise for users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Domimatch::Application.config.secret_key_base = 'a63818fea86bee5860ad3daa5c7c2b546a8df99429d8111e8ea9fba8ce302ca2f9a100c0ca6b168fa2aabc438407f37c423dfcef6bfd7c5e9263f0abfb9c22ce'
