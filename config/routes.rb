958a8d83c938fcfe8459fa919fe4dc895fcd49a0
Mike Adeleke <madeleke13@gmail.com>
2013-11-18 22:53:43 -0600

    Added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Domimatch::Application.routes.draw do
+  get "pages/home"
   resources :listings
 
   devise_for :users
@@ -6,7 +7,7 @@ Domimatch::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
