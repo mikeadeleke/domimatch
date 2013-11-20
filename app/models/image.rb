8796ae6e542f7bb55bfe9692200e6d9f978891dc
Mike Adeleke <madeleke13@gmail.com>
2013-11-20 16:15:44 -0600

    Added images to listings

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,4 @@
 class Image < ActiveRecord::Base
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 	belongs_to :listing
-	mount_uploader :image, ImagesUploader
->>>>>>> 95c325823f36b95e1a8dd4fe24e72afcd5e42224
->>>>>>> 2fe939c40e3049c92ea4b65adb575fa9dd2fa490
+	mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131120021904) do
+ActiveRecord::Schema.define(version: 20131120220323) do
 
   create_table "images", force: true do |t|
     t.string   "image"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20131120021904) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "requests", force: true do |t|
